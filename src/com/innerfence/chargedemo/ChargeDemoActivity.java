e46b4aa3dd77abb536074db01129ee45734d33bc
Toland Hon <github@krunk4ever.com>
2012-05-09 19:00:46 -0700

    launch ccterminal on click.

--- src/com/innerfence/chargedemo/ChargeDemoActivity.java
+++ src/com/innerfence/chargedemo/ChargeDemoActivity.java
@@ -1,18 +1,23 @@
 package com.innerfence.chargedemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class ChargeDemoActivity extends Activity
 {
+    final int CCTERMINAL_RESPONSE_CODE = 1;
+
     protected final View.OnClickListener _chargeButtonClickListener =
         new View.OnClickListener()
         {
             @Override
             public final void onClick( View v )
             {
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.innerfence.ccterminal");
+                startActivityForResult( intent, CCTERMINAL_RESPONSE_CODE );
             }
         };
 
