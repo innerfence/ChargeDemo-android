c4b4bcff05048dbb00e56761dc66d79a2eb4a71e
Toland Hon <github@krunk4ever.com>
2012-05-09 18:07:49 -0700

    hook up charge button to a listener

--- src/com/innerfence/chargedemo/ChargeDemoActivity.java
+++ src/com/innerfence/chargedemo/ChargeDemoActivity.java
@@ -2,13 +2,27 @@ package com.innerfence.chargedemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChargeDemoActivity extends Activity
 {
+    protected final View.OnClickListener _chargeButtonClickListener =
+        new View.OnClickListener()
+        {
+            @Override
+            public final void onClick( View v )
+            {
+            }
+        };
+
     @Override
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.main );
+
+        Button chargeButton = (Button)findViewById( R.id.charge_button );
+        chargeButton.setOnClickListener( _chargeButtonClickListener );
     }
 }
